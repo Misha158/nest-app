c6e66920b1b6c8a7f249f654c3c2c837fa22e53a
mykhail.svekolnykov <Mykhail.Svekolnykov.-ND@disney.com>
2023-09-15 12:47:46 +0300

    run nest g controller dog

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
+import { DogController } from './dog/dog.controller';
 
 @Module({
   imports: [CatModule],
-  controllers: [AppController],
+  controllers: [AppController, DogController],
   providers: [AppService],
 })
 export class AppModule {}
